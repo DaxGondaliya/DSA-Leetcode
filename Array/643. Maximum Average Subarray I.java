7d9924d274d313325a0831840766f0e0af7931d3
Daxgondaliya <ddgondaliya8826@gmail.com>
2025-12-29 13:44:11 +0530

    solved Maximum Average Subarray I

---
+++ Array/643. Maximum Average Subarray I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double max = 0;
+        double sum = 0;
+        for(int i=0 ; i<k ; i++){
+            sum += nums[i];
+        }
+        max = sum/k;
+        for(int i=k ; i<nums.length ; i++){
+            sum += nums[i] - nums[i-k];
+            max = sum/k>max ? sum/k : max;
+        }
+        return max;
+    }
+}
