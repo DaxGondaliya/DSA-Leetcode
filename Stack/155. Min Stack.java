09e800b3410a586806a90252158cafd5fdbe3c36
Daxgondaliya <ddgondaliya8826@gmail.com>
2026-01-20 14:30:31 +0530

    solved Min Stack

---
+++ Stack/155. Min Stack.java
@@ -0,0 +1,46 @@
+class MinStack {
+    private Stack<Integer> s;
+    private Stack<Integer> min;
+
+    public MinStack() {
+        s = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        s.push(val);
+        if (min.isEmpty() || val <= min.peek()){
+            min.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(s.peek().equals(min.peek())){
+            min.pop();
+        }
+        s.pop();
+    }
+    
+    public int top() {
+        if(s.isEmpty()){
+            return -1;
+        }
+        return s.peek();    
+    }
+    
+    public int getMin() {
+        if(min.isEmpty()){
+            return -1;
+        }
+        return min.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
