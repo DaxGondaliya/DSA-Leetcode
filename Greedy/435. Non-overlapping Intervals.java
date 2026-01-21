017f574e27a08c313d6203573a1df1f55c7c9460
Daxgondaliya <ddgondaliya8826@gmail.com>
2026-01-21 19:38:39 +0530

    solved Non-overlapping Intervals

---
+++ Greedy/435. Non-overlapping Intervals.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        /*use Integer.compare(a[i],b[i]) instead of a[i]-a[b] because might be 
+        overflow for bigger differance*/
+        Arrays.sort(intervals , (a,b)->Integer.compare(a[1],b[1]));
+        int count = 1;
+        int last = intervals[0][1];
+        for(int i=0 ; i<intervals.length ; i++){
+            if(intervals[i][0]>=last){
+                count++;
+                last = intervals[i][1];
+            }
+        }
+        return intervals.length-count;
+    }
+}
