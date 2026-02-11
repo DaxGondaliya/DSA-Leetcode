5f4d624ec0ec612d2f3a52634ef5e7d1a2933cd3
Daxgondaliya <ddgondaliya8826@gmail.com>
2026-02-11 10:46:33 +0530

    solved Successful Pairs of Spells and Potions

---
+++ Binary Search/2300. Successful Pairs of Spells and Potions.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int pairs[] = new int[spells.length];
+
+        Arrays.sort(potions);
+
+        for(int i=0 ; i<spells.length ; i++){
+            int spell = spells[i];
+            int left = 0;
+            int right = potions.length-1;
+
+            while(left<=right){
+                int mid = left + (right-left)/2;
+                long mul = (long)spell * potions[mid];
+
+                if(mul >= success){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            pairs[i] = potions.length-left;
+        }
+        return pairs;
+    }
+}
