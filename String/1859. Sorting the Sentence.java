725825fc11d31a8f9647aec95f5c117c9f56f138
Daxgondaliya <ddgondaliya8826@gmail.com>
2026-03-10 21:17:46 +0530

    solved Sorting the Sentence

---
+++ String/1859. Sorting the Sentence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String sortSentence(String s) {
+        String arr[] = s.split(" ");
+        String ans[] = new String[arr.length];
+
+        for(String w : arr){
+            int idx = w.charAt(w.length()-1) - '1';
+            ans[idx] = w.substring(0,w.length()-1);
+        }
+        return String.join(" " , ans);
+    }
+}
