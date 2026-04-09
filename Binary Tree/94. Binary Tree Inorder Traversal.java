95d13f9d316b36670d718fef9f61f17ecddc9b19
Daxgondaliya <ddgondaliya8826@gmail.com>
2026-04-09 22:27:27 +0530

    solved 94. Binary Tree Inorder Traversal

---
+++ Binary Tree/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+
+//         inorder(root, res);
+//         return res; 
+//     }
+//     private void inorder(TreeNode node, List<Integer> res) {
+//         if (node == null) {
+//             return;
+//         }
+//         inorder(node.left, res);
+//         res.add(node.val);
+//         inorder(node.right, res);
+//     } 
+// }
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Stack<TreeNode> st = new Stack<>();
+        while(true){
+            if(root != null){
+                st.push(root);
+                root = root.left;
+            }else{
+                if(st.isEmpty()){
+                    break;
+                }
+                root = st.pop();
+                ans.add(root.val);
+                root = root.right;
+            }
+        }
+        return ans;
+    } 
+}
